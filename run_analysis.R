1905669d843ed38bd9195bd77a98fc2ea36bec50
Andresfmurillo <43020285+Andresfmurillo@users.noreply.github.com>
2019-02-17 18:07:47 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,49 +1,49 @@
 library(dplyr)
 
 
-# read training data
+# read training folder
 train_subjects <- read.table("subject_train.txt")
 train_values <- read.table("X_train.txt")
 train_activity <- read.table("y_train.txt")
 
-# read test data
+# read test folder
 test_subjects <- read.table("subject_test.txt")
 test_values <- read.table("X_test.txt")
 test_activity <- read.table("y_test.txt"))
 
-# read features without convertin of text labels in to factors
+# read features file
 features <- read.table("features.txt")
 
-# read activity labels
+# read activity labels file
 activities <- read.table("activity_labels.txt")
 colnames(activities) <- c("Id", "Label")
 
-### *** 1. Merge the training and the test sets to create one data set *** ###
+# 1. Merge the train and test sets
 
 # create single data table
 Activity <- rbind(cbind(train_subjects, train_values, train_activity),cbind(test_subjects, test_values, test_activity))
 
-# remove single data tables in order to save memory
+# remove single data tables 
 rm(train_subjects, train_values, train_activity, test_subjects, test_values, test_activity)
 
-# give column names
+# name columns
 colnames(Activity) <- c("subject", features[, 2], "activity")
 
-### *** 2. Extract only the measurements on the mean and standard deviation for each measurement *** ###
+# 2. Extract mean and standard deviation 
 
 # select only the varibels, which we need according to their columns names
 Activity <- Activity[, grepl("subject|activity|mean|std", colnames(Activity))]
 
-### *** 3. Use descriptive activity names to name the activities in the data set *** ###
+# 3. Use descriptive activity to name the activities 
 
 Activity <- mutate(Activity,activity=factor(activity,levels = activities[,1],labels = activities[,2]))
 
-### *** 4. Appropriately label the data set with descriptive variable names *** ###
+# 4. Appropriately label the data set with descriptive variables
 
 # get column names
 Activity_col <- colnames(Activity)
 
-# remove special characters, clead the names and correct the type
+# remove special characters
 Activity_col <- gsub("[\\(\\)-]", "", Activity_col)
 Activity_col <- gsub("^f", "frequencyDomain", Activity_col)
 Activity_col <- gsub("^t", "timeDomain", Activity_col)
@@ -55,14 +55,11 @@ Activity_col <- gsub("mean", "Mean", Activity_col)
 Activity_col <- gsub("std", "StandardDeviation", Activity_col)
 Activity_col <- gsub("BodyBody", "Body", Activity_col)
 
-# replace the col-names with the new one
+# replace the colnames 
 colnames(Activity) <- Activity_col
 
-### *** Step 5. Create a second, independent tidy set with the average of each 
-#          variable for each activity and each subject *** ####
+# 5. Create a second, independent tidy set with the average of each variable for each activity and each subject
 
 # group by subject and activity and summarise using mean
 Activity_means <- Activity %>% group_by(subject, activity) %>% summarise_all(funs(mean))
-
-# output to file "tidy_data.txt"
 write.table(Activity_means, "tidy_data.txt", row.names = FALSE, quote = FALSE)
